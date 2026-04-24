7814768553da8516c47e4043065da982dce85d12
SebsCk <sebastianclark.estrada@gmail.com>
2026-04-24 21:34:41 +0800

    Fix CORS allow all origins

--- budgetcooks-backend/src/index.js
+++ budgetcooks-backend/src/index.js
@@ -14,7 +14,9 @@ const app  = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(cors({
-  origin: [process.env.CLIENT_URL, 'http://localhost:5173'],
+  origin: function(origin, callback) {
+    callback(null, true);
+  },
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
