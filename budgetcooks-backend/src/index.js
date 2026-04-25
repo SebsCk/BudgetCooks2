65a552e6c1668dcbe3c61f1b3c3fa7c422df3ed0
SebsCk <sebastianclark.estrada@gmail.com>
2026-04-25 10:44:09 +0800

    Handle OPTIONS preflight

--- budgetcooks-backend/src/index.js
+++ budgetcooks-backend/src/index.js
@@ -21,7 +21,7 @@ app.use(cors({
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
 }));
-
+app.options('*', cors());
 app.use(express.json());
 
 app.use('/auth',         authRoutes);
