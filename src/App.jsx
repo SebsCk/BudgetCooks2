dd0fafdaa3dc903c2d229ba258d923bfbd8fdd97
SebsCk <sebastianclark.estrada@gmail.com>
2026-04-26 12:24:15 +0800

    Add missing routes for Feed, Challenges, Categories, About

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { AuthProvider }   from './context/AuthContext'
-import Navbar             from './components/layout/Navbar'
-import Home               from './pages/Home'
-import Login              from './pages/Login'
-import Signup             from './pages/Signup'
-import AdminDashboard     from './pages/AdminDashboard'
-import ShoppingList       from './pages/ShoppingList'
-import MealPlanner        from './pages/MealPlanner'
+import { AuthProvider }    from './context/AuthContext'
+import Navbar              from './components/layout/Navbar'
+import Home                from './pages/Home'
+import Login               from './pages/Login'
+import Signup              from './pages/Signup'
+import AdminDashboard      from './pages/AdminDashboard'
+import ShoppingList        from './pages/ShoppingList'
+import MealPlanner         from './pages/MealPlanner'
+import FeedPage            from './pages/FeedPage'
+import ChallengesPage      from './pages/ChallengesPage'
+import CategoriesPage      from './pages/CategoriesPage'
+import AboutPage           from './pages/AboutPage'
 import './index.css'
 
 export default function App() {
@@ -16,6 +20,10 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/"              element={<Home />}            />
+          <Route path="/feed"          element={<FeedPage />}        />
+          <Route path="/challenges"    element={<ChallengesPage />}  />
+          <Route path="/categories"    element={<CategoriesPage />}  />
+          <Route path="/about"         element={<AboutPage />}       />
           <Route path="/login"         element={<Login />}           />
           <Route path="/signup"        element={<Signup />}          />
           <Route path="/admin"         element={<AdminDashboard />}  />
