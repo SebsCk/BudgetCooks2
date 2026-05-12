4a6a2d144277fc6b7836ddfd89d0499cb5909730
CasualMelonLover <francissebonga@gmail.com>
2026-05-12 09:46:57 +0800

    Change index file, allowing 10MB photo upload

--- budgetcooks-backend/src/index.js
+++ budgetcooks-backend/src/index.js
@@ -35,7 +35,8 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.options('*', cors(corsOptions))
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use('/auth',           authRoutes);
 app.use('/api/recipes',    recipeRoutes);
